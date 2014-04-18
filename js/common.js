e8978e056dc15ac9b428abe74b17fbe038fd8aa4
Splintex <godun.yevgen@gmail.com>
2014-04-18 14:03:33 +0300

    header,footer

--- js/common.js
+++ js/common.js
@@ -1,3 +1,43 @@
 $(document).ready(function() {
+	$(document).click(function() {
+        //$(".js-select-list").hide();
+        $(".js-select").removeClass("is-active");
+    });
+    function selectList() {
+        var select = $(".js-select");
+        var select_list = $(".js-select-list");
+        $("body").on("click", ".js-select", function(event){
+            if ($(this).hasClass("is-active")) {
+                select.removeClass("is-active");
+                //select_list.hide();
+            }
+            else {
+                select.removeClass("is-active");
+                //select_list.hide();
+                //$(this).find(".js-select-list").show();
+                $(this).addClass("is-active");
+            }
+            event.stopPropagation();
+        });
+        $("body").on("click", ".js-select-list a", function(event){
+            var id = $(this).parent().attr("data-id");
+            var html = $(this).html();
+            $(this).parents(".js-select").find(".js-select-text").html(html);
+            $(this).parents(".js-select").find(".js-select-input").val(id);
+            //$(this).parent().hide();
+            $(this).parents(".js-select").removeClass("is-active");
+            event.stopPropagation();
+        });
+        $(".js-more-list").on("click", function(event){
+        	$(this).parents(".js-select-list").addClass("has-scroll");
+        	$(this).parents(".js-select-list").find("li").removeAttr("hidden");
+        	$(this).parent().remove();
+	        event.stopPropagation();
+	    });
+    }  
+    selectList();
+    $("body").on("click", ".js-select", function(event){
+        event.stopPropagation();
+    });
 
 });
