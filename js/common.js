98b4de936a71028c81880e9465339adb5f45227c
Splintex <eugene.godin@gmail.com>
2014-04-23 13:44:28 +0300

    all pages

--- js/common.js
+++ js/common.js
@@ -55,83 +55,93 @@ $(document).ready(function() {
         event.stopPropagation();
     });
 	$(".js-validate-form").each(function(){
+		if ($(this).length > 0) {
 			$(this).validate({
 				errorClass: "has-error",
-			rules: {
-				firstname: "required",
-				lastname: "required",
-				username: {
-					required: true,
-					minlength: 2
+				rules: {
+					firstname: "required",
+					lastname: "required",
+					username: {
+						required: true,
+						minlength: 2
+					},
+					password: {
+						required: true,
+						minlength: 5
+					},
+					confirm_password: {
+						required: true,
+						minlength: 5,
+						equalTo: "#password"
+					},
+					email: {
+						required: true,
+						email: true
+					},
+					tel: {
+						required: true,
+						minlength: 2
+					},
+					message: {
+						required: true,
+						minlength: 2
+					}
 				},
-				password: {
-					required: true,
-					minlength: 5
-				},
-				confirm_password: {
-					required: true,
-					minlength: 5,
-					equalTo: "#password"
-				},
-				email: {
-					required: true,
-					email: true
-				},
-				tel: {
-					required: true,
-					minlength: 2
-				},
-				//agree: "required"
-			},
-			messages: {
-				firstname: "Вас так зовут?",
-				lastname: "У вас такая фамилия?",
-				// username: {
-				// 	required: "Please enter a username",
-				// 	minlength: "Your username must consist of at least 2 characters"
-				// },
-				password: {
-					required: "Пароли не совпадают",
-					minlength: "Минимум 5 символов",
-				},
-				confirm_password: {
-					required: "Пароли не совпадают",
-					minlength: "Минимум 5 символов",
-					equalTo: "Please enter the same password as above"
-				},
-				email: "Тут что-то не так :(",
-				//tel: "Телефон с ошибкой :(",
-				tel: {
-					required: "Телефон с ошибкой :(",
-					digits: true,
-					//minlength: "Your password must be at least 5 characters long",
-					//equalTo: "Please enter the same password as above"
-				},
-				//agree: "Please accept our policy"
-			}
-		});
+				messages: {
+					firstname: "Вас так зовут?",
+					lastname: "У вас такая фамилия?",
+					// username: {
+					// 	required: "Please enter a username",
+					// 	minlength: "Your username must consist of at least 2 characters"
+					// },
+					password: {
+						required: "Пароли не совпадают",
+						minlength: "Минимум 5 символов",
+					},
+					confirm_password: {
+						required: "Пароли не совпадают",
+						minlength: "Минимум 5 символов",
+						equalTo: "Please enter the same password as above"
+					},
+					email: "Тут что-то не так :(",
+					//tel: "Телефон с ошибкой :(",
+					tel: {
+						required: "Телефон с ошибкой :(",
+						digits: true,
+						//minlength: "Your password must be at least 5 characters long",
+						//equalTo: "Please enter the same password as above"
+					},
+					message: {
+						required: "Это Ваш вопрос?",
+					}
+					//agree: "Please accept our policy"
+				}
+			});
+		}
 	});
 	$(".js-enter-form").each(function(){
-		$(this).validate({
-			errorClass: "has-error",
-			rules: {
-				password: {
-					required: true,
-					minlength: 5
+		if ($(this).length > 0) {
+			$(this).validate({
+				errorClass: "has-error",
+				rules: {
+					password: {
+						required: true,
+						minlength: 5
+					},
+					email: {
+						required: true,
+						email: true
+					},
 				},
-				email: {
-					required: true,
-					email: true
-				},
-			},
-			messages: {
-				password: {
-					required: "Неверный пароль :(",
-					minlength: "Минимум 5 символов",
-				},
-				email: "Тут что-то не так :(",
-			}
-		});
+				messages: {
+					password: {
+						required: "Неверный пароль :(",
+						minlength: "Минимум 5 символов",
+					},
+					email: "Тут что-то не так :(",
+				}
+			});
+		}
 	});
 	   
 	$(".js-item-popup").on("click", function(event){
@@ -147,7 +157,7 @@ $(document).ready(function() {
        $(".js-accordion-title").addClass("is-active");
        $(".js-hidden-list .js-accordion-list").hide();
        $(".js-hidden-list .js-accordion-title").removeClass("is-active");
-        $(".js-accordion-title").click(function(){
+        $("body").on("click", ".js-accordion-title",function(){
             if ($(this).parent().hasClass("js-one-active")) {
                 $(".js-accordion-title").removeClass("is-active");
                 $(".js-accordion-list").slideUp("fast");
@@ -160,7 +170,7 @@ $(document).ready(function() {
             }
             
         });
-        $(".js-show-all-accordion").on("click",function(){
+        $("body").on("click", ".js-show-all-accordion",function(){
             if ($(this).hasClass("is-active")) {
                 $(".js-show-all-accordion").removeClass("is-active");
                 $(".js-accordion-list").slideUp("fast");
@@ -195,9 +205,22 @@ $(document).ready(function() {
 		return false;
         event.stopPropagation();
     });
-    $(".js-del-item").on("click", function(event){
+    $("body").on("click",".js-del-item", function(event){
     	$(this).parents(".item").remove();
+    	$(this).parents(".address").remove();
 		return false;
         event.stopPropagation();
     });
+    $(".js-select select").on("change", function(){
+    	var val = $(this).val();
+    	$(this).parents(".js-select").find(".input").text(val);
+    });
+    $(".js-add-address").on("click", function(){
+    	var counter = $(this).parents(".js-validate-form").find(".address").length;
+    	$(".js-new-address .js-accordion-title span").text(counter+1);
+    	var address = $(".js-new-address").html();
+    	$(".js-new-address-group").append(address);
+    	return false;
+    });
+
 });
